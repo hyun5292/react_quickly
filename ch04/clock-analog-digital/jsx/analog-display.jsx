db768c3e0224d5e98a7e3e7401a6a2ea4fe2163b
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-03-30 01:40:43 +0900

    ch04 완료
    4-1 React 컴포넌트의 상태란?
    4-2 상태 객체 다루기
    4-3 상태 객체와 속성
    4-4 상태비저장 컴포넌트
    4-5 상태비저장 컴포넌트와 상태저장 컴포넌트의 비교

---
+++ ch04/clock-analog-digital/jsx/analog-display.jsx
@@ -0,0 +1,59 @@
+const AnalogDisplay = function (props) {
+  let date = new Date(props.time);
+  let dialStyle = {
+    positon: "relative",
+    top: 0,
+    left: 0,
+    width: 200,
+    height: 200,
+    borderRadius: 20000,
+    borderStyle: "solid",
+    borderColor: "black",
+  };
+  let secondHandStyle = {
+    position: "relative",
+    top: 100,
+    left: 100,
+    border: "1px solid red",
+    width: "40%",
+    height: 1,
+    transform:
+      "rotate(" + ((date.getSeconds() / 60) * 360 - 90).toString() + ")deg",
+    transformOrigin: "0% 0%",
+    backgroundColor: "red",
+  };
+  let minuteHandStyle = {
+    position: "relative",
+    top: 100,
+    left: 100,
+    border: "1px solid grey",
+    width: "40%",
+    height: 3,
+    transform:
+      "rotate(" + ((date.getMinutes() / 60) * 360 - 90).toString() + ")deg",
+    transformOrigin: "0% 0%",
+    backgroundColor: "grey",
+  };
+  let hourHandStyle = {
+    position: "relative",
+    top: 100,
+    left: 100,
+    border: "1px solid grey",
+    width: "40%",
+    height: 3,
+    transform:
+      "rotate(" + ((date.getHours() / 60) * 360 - 90).toString() + ")deg",
+    transformOrigin: "0% 0%",
+    backgroundColor: "grey",
+  };
+
+  return (
+    <div>
+      <div style={dialStyle}>
+        <div style={secondHandStyle} />
+        <div style={minuteHandStyle} />
+        <div style={hourHandStyle} />
+      </div>
+    </div>
+  );
+};
