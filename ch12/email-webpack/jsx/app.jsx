f0f3390ff8588c1f87e51165d38d8c24b96b65a3
박수현 <57443458+hyun5292@users.noreply.github.com>
2023-04-10 20:03:00 +0900

    ch12 완료
    12.1 Webpack의 역할
    12.2 프로젝트에 Webpack 적용하기
    12.3 코드 모듈화
    12.4 Webpack의 실행과 빌드 테스트
    12.5 핫 모듈 대체

---
+++ ch12/email-webpack/jsx/app.jsx
@@ -0,0 +1,6 @@
+import "../css/main.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Content from "./content.jsx";
+
+ReactDOM.render(<Content />, document.getElementById("content"));
